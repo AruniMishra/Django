f20e5d4ebf05426d7397d48817acb6050b6b78a4
Aruni Mishra <arunimishramsit@gmail.com>
2022-01-15 23:50:18 +0100

    django

--- Kickstart/playground/views.py
+++ Kickstart/playground/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def say_hello(request):
-    return HttpResponse('hello world')
+    #return HttpResponse('hello world')
+    return render(request, 'hello.html')
