16a74a87882cd4e129329808fb8aa1505b7553a2
Aruni Mishra <arunimishramsit@gmail.com>
2022-01-24 13:03:40 +0100

    Django

--- Kickstart/playground/views.py
+++ Kickstart/playground/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def calculate():
+    x=1
+    y=2
+    return x + y
+
 # Create your views here.
 
 def say_hello(request):
     #return HttpResponse('hello world')
-    return render(request, 'hello.html')
+    a = 1
+    b = 2
+    a = calculate()
+    return render(request, 'hello.html', {'name':'aruni'})
